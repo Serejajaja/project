3e3e3598d2efae4cc4f2fe6cf51214fa2a79391c
Сергей Е <ccrazyang@gmail.com>
2024-01-19 14:47:06 +0300

    Первичная загрузка на проверку логики движения

--- handlers/default_handlers/help.py
+++ handlers/default_handlers/help.py
@@ -2,11 +2,11 @@ from telebot.types import Message
 
 from config_data.config import DEFAULT_COMMANDS
 from loader import bot
-from keyboards.inline.inline_button import markup_line
+from keyboards.reply.reply_button import markup_button
 
 
 @bot.message_handler(commands=["help"])  # задаем команды на которые бот будет реагировать
 def bot_help(message: Message):
-    markup = markup_line()
+    markup = markup_button()
     text = [f"/{command} - {desk}" for command, desk in DEFAULT_COMMANDS]
     bot.reply_to(message, "\n".join(text), parse_mode='html', reply_markup=markup)
--- keyboards/reply/reply_button.py
+++ keyboards/reply/reply_button.py
@@ -2,7 +2,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def markup_button():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
     low = KeyboardButton('/low')
     high = KeyboardButton('/high')
     custom = KeyboardButton('/custom')
